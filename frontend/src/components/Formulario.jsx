8b57809ad5a93c2df57e489d09fcd415e6e7aea9
Delei09 <vanderlei_09@outlook.com>
2021-05-31 16:40:58 -0300

    form crud

--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Formulario.css'
+import axios from 'axios'
+
+const dadosInicial = {
+        nome : '' ,
+        email : ''
+}
 
 const Formulario = props =>{
+
+    const [ form , setForm ] = useState(dadosInicial)
+
+    function input(e){
+        const {name , value} = e.target 
+        setForm({...form , [name] : value})
+        console.log(form)
+    }
+    function salvar(e){
+        e.preventDefault();
+        console.log(form)
+    }
 
 
 
     return(
-        <form className = ''>
-            <label className = 'form-label' >Digite seu nome</label>
-            <input className = 'form-control' type = 'text' placeholder = 'Digite seu Nome'/>
-            <input className = 'form-control' type = 'text' placeholder = 'Digite seu Email'/>
+        <form className = 'container' onSubmit = {salvar}>
+            <input name = 'nome' className = 'form-control' type = 'text'
+             placeholder = 'Digite seu Nome' onChange = {input}/>
+            <input name = 'email' className = 'form-control' type = 'text'
+             placeholder = 'Digite seu Email' onChange = {input}/>
+            <button  className = 'btn btn-primary '>Salvar</button>
         </form>
     )
 }
-
 export default Formulario
