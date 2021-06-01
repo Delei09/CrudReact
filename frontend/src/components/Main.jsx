b8f8191ab9e2206dd2b77d85e5fdea8fc3cab181
Delei09 <vanderlei_09@outlook.com>
2021-06-01 15:14:09 -0300

    s

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -5,7 +5,7 @@ import './Main.css'
 const Main = (props) => {
     return(
         <main className="main">
-            <h2>Meu primeiro Crud feito com React</h2>
+            <h2> Crud feito com React</h2>
             <Formulario />
         </main>
     )
